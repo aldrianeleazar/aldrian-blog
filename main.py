64cf43a95c3077044fcfc2f5c96c384e53ac85b5
Aldrian Eleazar <aldrianeleazar0109@gmail.com>
2022-12-29 19:40:24 +0800

    comment db.create

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="blog_comments")
     text = db.Column(db.Text, nullable=False)
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
